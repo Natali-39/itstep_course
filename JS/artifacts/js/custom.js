cea7ce092dd0d41743d5c8e78e802ac466f5deea
harchockk <urao54840@gmail.com>
2025-10-20 21:49:41 +0300

    доработали maket7 - валидацию модальных окан

--- JS/artifacts/js/custom.js
+++ JS/artifacts/js/custom.js
@@ -41,9 +41,52 @@ $(document).ready(function(){
     totalPages = Math.ceil(totalCards / options.items);
 
     setSliderPagination()
+
+   // $("#form-login").validate();
+
+   const loginForm = document.querySelector('#form-login');
+
+   if(loginForm) {
+        loginForm.addEventListener('submit', function(event) {
+            if (!this.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+      
+            this.classList.add('was-validated');
+        });
+   }
 });
 
 function validate(){
-    return true;
+    let isValid = true;
+
+    let form = document.forms[0];
+    let name = form.elements[0];
+    let email = form.elements[1];
+
+    //let str = "hi! my name is Natali.";
+    //let regax = /\sNatali/; //\s - пробельный симвал
+    //let regax = /\d{2,}/; // симвал цифр {кол-во цифр ,-означает диапозон}
+
+    let name_regax = /^[A-Za-zА-Яа-я]{2,20}\s*[A-Za-zА-Яа-я]{0,20}$/;
+
+    let email_regax = /^[a-zA-Z_\-0-9]+@[a-zA-Z_\-0-9]+\.[a-z]{2,3}$/;
+
+    name.classList.remove(`is-invalid`);
+    if(!name_regax.test(name.value)) {
+        name.classList.add(`is-invalid`);
+        isValid = false;
+    }
+// второй вариант
+
+    if(!email_regax.test(email.value)) {
+        email.classList.add(`is-invalid`);
+        isValid = false;
+    }else {
+        email.classList.remove(`is-invalid`);
+    }
+
+    return isValid;
 }
 
