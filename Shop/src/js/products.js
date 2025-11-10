6033580b3d6a28a45b465479db9356ed17f17d5a
nataniks39 <nataniks39@gmail.com>
2025-11-10 21:48:53 +0300

    создали ИМ shop

---
+++ Shop/src/js/products.js
@@ -0,0 +1,11 @@
+const products = '[{"id": 1, "name": "товар 1", "description": "Это товар 1", "price": 1234, "image": "https://cartum.md/content/images/43/trending-products-for-dropshipping-84638301035807.jpg"}, {"id": 2, "name": "товар 2", "description": "Это товар 2", "price": 1234, "image": "https://cartum.md/content/images/43/trending-products-for-dropshipping-84638301035807.jpg"}, {"id": 3, "name": "товар 3", "description": "Это товар 3", "price": 1234, "image": "https://cartum.md/content/images/43/trending-products-for-dropshipping-84638301035807.jpg"}, {"id": 4, "name": "товар 4", "description": "Это товар 4", "price": 1234, "image": "https://cartum.md/content/images/43/trending-products-for-dropshipping-84638301035807.jpg"}, {"id": 5, "name": "товар 5", "description": "Это товар 5", "price": 1234, "image": "https://cartum.md/content/images/43/trending-products-for-dropshipping-84638301035807.jpg"}, {"id": 6, "name": "товар 6", "description": "Это товар 6", "price": 1234, "image": "https://cartum.md/content/images/43/trending-products-for-dropshipping-84638301035807.jpg"}]';
+
+
+
+
+
+
+
+
+
+export default products;
